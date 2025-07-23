1c58a8b1722bdaba186db6237591593d5a0054b3
Rizzu <rijvank118@gmail.com>
2025-07-23 10:58:59 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ dotenv.config({
 })
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 7000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+
+.catch( (err) => {
+    console.log("MONGODB connection failed !!! ", err)
+})
 
 
 
